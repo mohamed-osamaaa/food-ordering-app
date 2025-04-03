5bc9e2660fdb3dadc2fdd285dd90d15055040181
Mohamed Osama <141563962+mohamed-osamaaa@users.noreply.github.com>
2025-04-03 11:19:32 +0200

    first commit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,9 @@
-export const ARTICLE_PER_PAGE = 6;
+export const USER_PER_PAGE = 6;
 
 const PRODUCTION_DOMAIN = "https://cloud-hosting-project-course.vercel.app";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 
-export const DOMAIN = process.env.NODE_ENV === 'production' 
-    ? PRODUCTION_DOMAIN
-    : DEVELOPMENT_DOMAIN;
+export const DOMAIN =
+    process.env.NODE_ENV === "production"
+        ? PRODUCTION_DOMAIN
+        : DEVELOPMENT_DOMAIN;
